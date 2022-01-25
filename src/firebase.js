015ce4c53952b3dfe8367b7727de146fdb5e6692
bsheranian <bsheranian1@gmail.com>
2022-01-25 12:56:11 -0700

    remove firebase API Keys

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDjJcpOwtXPOZpsT90uYCUheyNgXXIXilg",
-  authDomain: "stride-f1ae6.firebaseapp.com",
-  projectId: "stride-f1ae6",
-  storageBucket: "stride-f1ae6.appspot.com",
-  messagingSenderId: "63539191224",
-  appId: "1:63539191224:web:8f0ad0c1886d804462834d",
-  measurementId: "G-YKBDC9HPJT"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 
