1139d40d4cfcca41bda9988a58277fd5d3d4826d
bsheranian <bsheranian1@gmail.com>
2022-01-25 12:47:38 -0700

    update to work with firebase 9.6.4

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,21 @@
-import firebase from "firebase/app"
-import "firebase/auth"
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
 
-const app = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
-})
+const firebaseConfig = {
+  apiKey: "AIzaSyDjJcpOwtXPOZpsT90uYCUheyNgXXIXilg",
+  authDomain: "stride-f1ae6.firebaseapp.com",
+  projectId: "stride-f1ae6",
+  storageBucket: "stride-f1ae6.appspot.com",
+  messagingSenderId: "63539191224",
+  appId: "1:63539191224:web:8f0ad0c1886d804462834d",
+  measurementId: "G-YKBDC9HPJT"
+};
 
+
+const app = firebase.initializeApp(firebaseConfig);
 export const auth = app.auth()
 export default app
+
+
+
+
